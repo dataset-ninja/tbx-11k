2295d68e4656cc2bf7cb9045203bb85b3d3ef399
erastov-alex <erastov.alex@gmail.com>
2023-09-15 00:32:22 +0300

    upd settings,convert,getignore,req,ABSTRACT

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,14 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from pycocotools.coco import COCO
+from supervisely.io.fs import get_file_name, get_file_size
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -52,7 +54,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -60,21 +63,136 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    dataset_path = "TBX11K"
+    img_path = os.path.join(dataset_path, "imgs")
+    batch_size = 50
 
-    # ... some code here ...
+    inst_test = os.path.join("TBX11K", "annotations", "json", "all_test.json")
+    inst_train = os.path.join("TBX11K", "annotations", "json", "TBX11K_train.json")
+    inst_val = os.path.join("TBX11K", "annotations", "json", "TBX11K_val.json")
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    instances = {"test": inst_test, "train": inst_train, "val": inst_val}
+    # instances = { "train": inst_train, "val": inst_val}
 
-    # return project
+    tag_names = [
+        "healthy",
+        "sick_but_non-tb",
+        "active_tb",
+        "latent_tb",
+        "active&latent_tb",
+        "uncertain_tb",
+    ]
+    """
+    more about:
 
+    ***healthy***
+    if "health" in img_path
+    ***sick_but_non-tb***
+    if "sick" in img_path
+    ***active_tb***
+    if label_name != "ObsoletePulmonaryTuberculosis"
+    ***latent_tb***
+    if label_name == "ObsoletePulmonaryTuberculosis"
+    ***active&latent_tb***
+    if "ObsoletePulmonaryTuberculosis" in labels and "PulmonaryTuberculosis" or "ActiveTuberculosis" in labels
+    ***uncertain_tb***
+    no data for test. Check README of orginal dataset.
+    """
 
+    instance_coco = COCO(inst_train)
+    categories = instance_coco.cats
+
+    def create_ann(image_path, img_dict):
+        image_id = img_dict[image_path]
+        labels = []
+        tag_value = None
+        img_height = images[image_id]["height"]
+        img_wight = images[image_id]["width"]
+        image_name = os.path.basename(image_path)
+        if "s" in image_name:
+            tag_value = "sick_but_non-tb"
+        elif "h" in image_name:
+            tag_value = "healthy"
+        for label in annotations[image_id]:
+            bbox = label["bbox"]
+            cat_id = label["category_id"]
+            label_name = categories[cat_id]["name"]
+            if label_name == "ObsoletePulmonaryTuberculosis":
+                tag_value = "latent_tb"
+            else:
+                tag_value = "active_tb"
+            left, top, right, bottom = (
+                bbox[0],
+                bbox[1],
+                bbox[2] + bbox[0],
+                bbox[3] + bbox[1],
+            )
+            geometry = sly.Rectangle(top, left, bottom, right)
+            obj_class = meta.get_obj_class(label_name)
+            curr_label = sly.Label(geometry, obj_class)
+            labels.append(curr_label)
+        if len(labels) > 1:
+            for label in labels:
+                if (
+                    label.obj_class.name == "ObsoletePulmonaryTuberculosis"
+                    and tag_value == "active_tb"
+                ):
+                    tag_value = "active&latent_tb"
+                elif (
+                    label.obj_class.name != "ObsoletePulmonaryTuberculosis"
+                    and tag_value == "latent_tb"
+                ):
+                    tag_value = "active&latent_tb"
+        tags = [sly.Tag(tag_meta) for tag_meta in tag_metas if tag_meta.name == tag_value]
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+    obj_classes = [sly.ObjClass(categories[cat]["name"], sly.Rectangle) for cat in categories]
+
+    tag_metas = [sly.TagMeta(name, sly.TagValueType.NONE) for name in tag_names]
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=obj_classes, tag_metas=tag_metas)
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset_test = api.dataset.create(project.id, "test", change_name_if_conflict=True)
+    dataset_val = api.dataset.create(project.id, "val", change_name_if_conflict=True)
+    dataset_train = api.dataset.create(project.id, "train", change_name_if_conflict=True)
+
+    progress = sly.Progress("Create dataset {}".format("ds0"), 10000)
+
+    def norm_path(path):
+        new_path = ""
+        path = path.split("/")
+        for el in path:
+            new_path = os.path.join(new_path, el)
+        return os.path.join(img_path, new_path)
+
+    for inst in instances:
+        if inst == "val":
+            dataset = dataset_val
+        elif inst == "train":
+            dataset = dataset_train
+        else:
+            dataset = dataset_test
+        instance_coco = COCO(instances[inst])
+        categories = instance_coco.cats
+        images = instance_coco.imgs
+        indexes = list(images)
+        annotations = instance_coco.imgToAnns
+        for index_batch in sly.batched(indexes, batch_size=batch_size):
+            img_paths = [norm_path(images[i]["file_name"]) for i in index_batch]
+            img_keys = [images[i]["id"] for i in index_batch]
+            img_names_batch = [os.path.basename(img_path) for img_path in img_paths]
+            img_dict = {img[0]: img[1] for img in zip(img_paths, img_keys)}
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_paths)
+            img_ids = [im_info.id for im_info in img_infos]
+            anns_batch = [create_ann(image_path, img_dict) for image_path in img_paths]
+            api.annotation.upload_anns(img_ids, anns_batch)
+            progress.iters_done_report(len(img_names_batch))
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,42 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "TBX11K"
+PROJECT_NAME_FULL: str = "Tuberculosis X-ray"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.CC_BY_4_0()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [
+    Research.Medical(),
+    Industry.Medical(),
+]
+CATEGORY: Category = Category.Medical()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2019-11-30"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://mmcheng.net/tb/"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 4177587
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/tbx-11k"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = [
+    "https://drive.google.com/file/d/1r-oNYTPiPCOUzSjChjCIYTdkjBTugqxR/view?usp=sharing"
+]
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,18 +56,41 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = [
+    "https://openaccess.thecvf.com/content_CVPR_2020/papers/Liu_Rethinking_Computer-Aided_Tuberculosis_Diagnosis_CVPR_2020_paper.pdf"
+]
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
 REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
-CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
+CITATION_URL: Optional[str] = ["https://mmcheng.net/tb/"]
+AUTHORS: Optional[List[str]] = [
+    "Yun Liu",
+    "Yu-Huan Wu",
+    "Yunfeng Ban",
+    "Huifang Wang",
+    "Ming-Ming Cheng",
+]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
+    "Nankai University",
+    "InferVision",
+]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
+    "https://en.nankai.edu.cn/",
+    "https://global.infervision.com/",
+]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {
+    "categories": [
+        "healthy",
+        "sick_but_non-tb",
+        "active_tb",
+        "latent_tb",
+        "active&latent_tb",
+        "uncertain_tb",
+    ]
+}
 TAGS: Optional[List[str]] = None
 
 
